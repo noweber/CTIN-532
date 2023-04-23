5c21fb4f244a7ff37e1e716bae437164074ea5c1
Jackie-LM <lmx1342@gmail.com>
2023-04-23 01:17:00 -0700

    prefab updates

---
+++ CTIN-532/Assets/_Script/Visual/SortOrder.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortOrder : MonoBehaviour
+{
+    public bool isMoveing = false;
+
+    private SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject + ": " +(transform.position.z * 1000));
+        //Debug.Log(gameObject + ": " + (int)(transform.position.z * 1000));
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.sortingOrder = -(int)(transform.position.z * 1000);
+        //Debug.Log(renderer.sortingOrder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoveing)
+        {
+            renderer.sortingOrder = (int)(transform.position.z * 100);
+        }
+    }
+}
